9ec1aeb6097e7fb517ed0b3765d13d69e8133383
David Tenenbaum <davidtenenbaum@Davids-MacBook-Pro.local>
2020-01-30 15:41:05 -0500

    Updates

---
+++ precache-manifest.4301f83cbb7c8cb4b87c1d6ed8a4a427.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be189e896f203ebd520805726465687",
+    "url": "/demo-website/index.html"
+  },
+  {
+    "revision": "1411e043633d32001902",
+    "url": "/demo-website/static/css/main.9fbac457.chunk.css"
+  },
+  {
+    "revision": "b634efaefc9397b93560",
+    "url": "/demo-website/static/js/2.48c7cd7e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/demo-website/static/js/2.48c7cd7e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1411e043633d32001902",
+    "url": "/demo-website/static/js/main.9d74daaf.chunk.js"
+  },
+  {
+    "revision": "5a49b5e0fbff94691406",
+    "url": "/demo-website/static/js/runtime-main.0ea0495f.js"
+  }
+]);
