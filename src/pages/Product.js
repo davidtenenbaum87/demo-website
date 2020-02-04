9acbeeadfa178a5cffc429c68441f501d42e9282
David Tenenbaum <davidtenenbaum@Davids-MacBook-Pro.local>
2020-02-04 10:07:46 -0500

    dy-div test

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class Product extends Component {
     constructor(props) {
@@ -26,27 +26,29 @@ class Product extends Component {
   
     render() {
         return (
-            <div className="product">
-                <div className="product-details">
-                    <div className="product-image" id={this.productData.sku}>
-                        <img src={this.productData.image_url} alt={this.productData.category} id={this.productData.sku} />
-                    </div>
-                    <div className="product-data">
-                        <div className="product-name">
-                            <p>{this.productData.name}</p>
+            <Fragment>
+                <div className="product">
+                    <div className="product-details">
+                        <div className="product-image" id={this.productData.sku}>
+                            <img src={this.productData.image_url} alt={this.productData.category} id={this.productData.sku} />
                         </div>
-                        <div className="product-price">
-                            <p>${this.productData.price}</p>
+                        <div className="product-data">
+                            <div className="product-name">
+                                <p>{this.productData.name}</p>
+                            </div>
+                            <div className="product-price">
+                                <p>${this.productData.price}</p>
+                            </div>
                         </div>
                     </div>
+                    {this.props.location.className === "product-item" ? 
+                        <button className="product-btn-add-cart" onClick={(e) => this.props.location.handleAddToCart(e, this.productData.sku)} >ADD TO CART</button>
+                    :
+                        null
+                    }
                 </div>
-                {this.props.location.className === "product-item" ? 
-                    <button className="product-btn-add-cart" onClick={(e) => this.props.location.handleAddToCart(e, this.productData.sku)} >ADD TO CART</button>
-                :
-                    null
-                }
                 <div className="dy-div"></div>
-            </div>
+            </Fragment>
         )
     }
 }
