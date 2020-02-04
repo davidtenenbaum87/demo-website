3a69cc3cd56fc7f3711b8b9c1a4ae6c09cb75121
David Tenenbaum <davidtenenbaum@Davids-MacBook-Pro.local>
2020-02-04 10:08:11 -0500

    Updates

--- precache-manifest.3f6970a459d567d8fe88fac6fce09024.js
+++ precache-manifest.3f6970a459d567d8fe88fac6fce09024.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "97710be167bf518b3cb6170be91cdc85",
+    "revision": "5ae774d89420e55f95c0671e062357d0",
     "url": "/demo-website/index.html"
   },
   {
-    "revision": "dd68e2390494da340aa4",
-    "url": "/demo-website/static/css/main.9fbac457.chunk.css"
+    "revision": "ddf41521a2563aa2ba0e",
+    "url": "/demo-website/static/css/main.7186a9f0.chunk.css"
   },
   {
     "revision": "eef5ada6ff27a2558579",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/demo-website/static/js/2.4f2f7989.chunk.js.LICENSE"
   },
   {
-    "revision": "dd68e2390494da340aa4",
-    "url": "/demo-website/static/js/main.746127dd.chunk.js"
+    "revision": "ddf41521a2563aa2ba0e",
+    "url": "/demo-website/static/js/main.1846c5ac.chunk.js"
   },
   {
     "revision": "5a49b5e0fbff94691406",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/demo-website/precache-manifest.6ab5567052cd458630a19b81ede57bc2.js"
+  "/demo-website/precache-manifest.3f6970a459d567d8fe88fac6fce09024.js"
 );
 
 self.addEventListener('message', (event) => {
